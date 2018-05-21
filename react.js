1f18f31c6896d22aa5b049b431a0479f8a5d0908
ZiYuan <zhangziyuan@meicai.cn>
2018-05-21 20:27:05 +0800

    feat: 实现 diff 算法

--- react.js
+++ react.js
@@ -105,7 +105,10 @@ function renderComponent(component) {
 		component.componentWillUpdate();
 	}
 
-	base = _render(renderer);
+	// base = _render(renderer);
+
+	base = diff(component.base, renderer);
+
 
 	if (component.base) {
 		if (component.componentDidUpdate) component.componentDidUpdate();
@@ -147,26 +150,210 @@ const ReactDom = {
 	}
 }
 
+/**
+	* diff(dom, vnode)
+	* @param {HTMLElement} dom 真实DOM
+	* @param {vnode} vnode 虚拟DOM
+	* @returns {HTMLElement} 更新后的DOM
+	*
+*/
 
-function Welcome(props) {
-	return <h1>Hello, {props.name} </h1>
+/**
+	* 首先可以先回忆一下虚拟DOM的结构
+	* 文本、原生节点、 组件
+	*	原生DOM
+	*	{
+	*   tag: 'div',
+	*   attrs: {
+	*    className: 'container' 
+	* 	},
+	*   children: []
+	*	}
+	* 文本
+	* "hello, world"
+	* 组件的vnode
+	* {
+	* 	tag: ComponentConstructor,
+	*   attrs: {
+	*   	className: 'container'
+	*		}
+	*   children: []
+	* }
+*/
+
+function diff(dom, vnode) {
+
+	if (typeof vnode === 'string') {
+		if (dom && dom.nodeType === 3) {
+			if (dom.textContent !== vnode) {
+				dom.textContent = vnode;
+			}
+		} else {
+			const out = document.createTextNode(vnode);
+			if (dom && dom.parentNode) {
+				dom.parentNode.replaceChild(out, dom);
+			}
+		}
+		return out;
+	}
+
+	if (!dom || dom.nodeName.toLowerCase() !== vnode.tag.toLowerCase()) {
+		out = document.createElement(vnode.tag);
+		if (dom) {
+			[...dom.childNodes].map(out.appendChild);
+
+			if (dom.parentNode) {
+				dom.parentNode.replaceChild(out, dom);
+			}
+		}
+	}
+
+
+// 这里是不是 dom 啊？一脸懵逼
+	if (vnode.children && vnode.children.length > 0 || (out.appendChild && out.childNodes.length > 0)) {
+		diffChildren(out, vnode.children);
+	}
 }
 
-function App() {
-	return (
-		<div>
-			<Welcome name="Sara" />
-			<Welcome name="Cahal" />
-			<Welcome name="Edite" />
-		</div>
-	)
+
+function diffChildren(dom, vchildren) {
+	const domChildren = dom.childNodes;
+	const children = [];
+	const keyed = {};
+
+// 将有key的节点和没有key的节点分开
+	if (domChildren.length > 0) {
+		for (let i = 0; i < domChildren.length; i++) {
+			const child = domChildren[i];
+			const key = child.key;
+			if (key) {
+				keyed[key] = child;
+			} else {
+				children.push(child);
+			}
+		}
+	}
+
+	if (vchildren && vchildren.length > 0) {
+		let min = 0;
+		let childrenLen = children.length;
+
+		for (let i = 0; i < vchildren.length; i ++) {
+			const vchild = vchildren[i];
+			const key = vchild.key;
+			let child;
+
+			if (key) {
+				child = keyed[key];
+				keyed[key] = undefined;
+			} else if (min < childrenLen) {
+				for (let j = min; j < childrenLen; j ++) {
+					let c = children[j];
+					if (c && isSameNodeType(c, vchild)) {
+						child = c;
+						children[j] = undefined;
+						if (j === children2 - 1) childrenLen --;
+						if (j === min) min ++;
+						break;
+					}
+				}
+			}
+
+			child = diff(child, vchild);
+
+			const f = domChildren[i];
+			if (child && child !== dom && child !== f) {
+				if (!f) {
+					dom.appendChild(child);
+				} else if (child === f.nextSibling) {
+					removeNode(f);
+				} else {
+					dom.insertBefore(child, f);
+				}
+			}
+		}
+	}
 }
 
-const element = <Welcome name="Sara" />;
-const appElement = <App />;
 
+function diffComponent(dom, vnode) {
+	let c = dom && dom._component;
+	let oldDom = dom;
+
+	if (c && c.constructor === vnode.tag) {
+		setComponentProps(c, vnode.attrs);
+		dom = c.base;
+	} else {
+		if (c) {
+			unmountComponents(c);
+			oldDom = null;
+		}
+		c = createComponent(vnode.tag, vnode.attrs);
+
+		setComponentProps(c, vnode.attrs);
+		dom = c.base;
+
+		if (oldDom && dom !== oldDom) {
+			oldDom._component = null;
+			removeNode(oldDom);
+		}
+	}
+	return dom;
+}
+
+
+
+function diffAttributes(dom, vnode) {
+	const old = {};
+	const attrs = vnode.attrs;
+	for (let i = 0; i < dom.length; i++) {
+		const attr = dom.attributes[i];
+		old[attr.name] = attr.value;
+	}
+
+	for (let name in old) {
+		if (!(name in attrs)) {
+			setAttribute(dom, name, attrs[name]);
+		}
+	}
+}
+
+
+class Counter extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+          num: 1
+      }
+  }
+
+  onClick() {
+      this.setState( { num: this.state.num + 1 } );
+  }
+
+  render() {
+      return (
+          <div>
+              <h1>count: { this.state.num }</h1>
+              <button onClick={ () => this.onClick()}>add</button>
+          </div>
+      );
+  }
+}
 
 ReactDom.render(
-	appElement,
+	<Counter />,
 	document.getElementById('root'),
 );
+
+
+
+
+
+
+
+
+
+
+
+
