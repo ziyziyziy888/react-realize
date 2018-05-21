765981908a545cbc99bd1700adbdd8554500f752
ZiYuan <zhangziyuan@meicai.cn>
2018-05-21 18:30:50 +0800

    feat: 实现 React 组件和生命周期

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
-
+import React from './react.js';
+import ReactDom from './react.js';
 // ReactDom.render(
 // 	<h2>Hello, world</h2>,
 // 	document.getElementById('root')
 // )
 
-function tick() {
-	const element = (
-		<div>
-			<h1>Hello, world</h1>
-			<h2>It is {new Date().toLocaleTimeString()}.</h2>
-		</div>
-	);
-	ReactDom.render(
-		element,
-		document.getElementById('root'),
-	)
-}
+// function tick() {
+// 	const element = (
+// 		<div>
+// 			<h1>Hello, world</h1>
+// 			<h2>It is {new Date().toLocaleTimeString()}.</h2>
+// 		</div>
+// 	);
+// 	ReactDom.render(
+// 		element,
+// 		document.getElementById('root'),
+// 	)
+// }
 
-setInterval(tick, 1000);
-
-
+// setInterval(tick, 1000);
 
 
 
--- react.js
+++ react.js
@@ -9,9 +9,22 @@ function createElement(tag, attrs, ...children) {
 }
 
 function render(vnode, container) {
+	return container.appendChild(_render(vnode));
+}
+
+function _render(vnode) {
+	if (vnode === undefined || vnode === null || typeof vnode === 'boolean') vnode = '';
+	if (typeof vnode === 'number') vnode = String(vnode);
+
 	if (typeof vnode === 'string') {
 		const textNode = document.createTextNode(vnode);
-		return container.appendChild(textNode);
+		return textNode;
+	}
+
+	if (typeof vnode.tag === 'function') {
+		const component = createComponent(vnode.tag, vnode.attrs);
+		setComponentProps(component, vnode.attrs);
+		return component.base;
 	}
 
 	const dom = document.createElement(vnode.tag);
@@ -22,12 +35,9 @@ function render(vnode, container) {
 			setAttribute(dom, key, value);
 		});
 	}
-
 // 递归渲染子节点
 	vnode.children.forEach(child => render(child, dom));
-
-
-	return container.appendChild(dom);
+	return dom;
 }
 
 function setAttribute(dom, name, value) {
@@ -56,6 +66,75 @@ function setAttribute(dom, name, value) {
 	}
 }
 
+function createComponent(component, props) {
+	let instance;
+
+// 如果是类定义组件， 则直接返回实例， 否则先将其扩展为类定义组件
+	if (component.prototype && component.prototype.render) {
+		instance = new component(props);
+	} else {
+		instance = new Component(props);
+		instance.constructor = component;
+		instance.render = function() {
+			return this.constructor(props);
+		}
+	}
+
+	return instance;
+}
+
+
+function setComponentProps(component, props) {
+	if (!component.base) {
+		if (component.componentWillMount) component.componentWillMount();
+	} else if (component.componentWillReceiveProps) {
+			component.componentWillReceiveProps(props);
+	}
+
+	component.props = props;
+
+	renderComponent(component);
+}
+
+
+function renderComponent(component) {
+	let base;
+	const renderer = component.render();
+
+	if (component.base && component.componentWillUpdate) {
+		component.componentWillUpdate();
+	}
+
+	base = _render(renderer);
+
+	if (component.base) {
+		if (component.componentDidUpdate) component.componentDidUpdate();
+	} else if (component.componentDidMount) {
+		component.componentDidMount();
+	}
+
+	if (component.base && component.base.parentNode) {
+		component.base.parentNode.replaceChild(base, component.base);
+	}
+
+	component.base = base;
+	base._component = component;
+}
+
+
+class Component {
+	constructor(props = {}) {
+		this.state = {};
+		this.props = props;
+	}
+
+// 暂时不去实现异步的setState
+	setState(stateChange) {
+		Object.assign(this.state, stateChange);
+		renderComponent(this);
+	}
+}
+
 
 const React = {
 	createElement,
@@ -68,3 +147,26 @@ const ReactDom = {
 	}
 }
 
+
+function Welcome(props) {
+	return <h1>Hello, {props.name} </h1>
+}
+
+function App() {
+	return (
+		<div>
+			<Welcome name="Sara" />
+			<Welcome name="Cahal" />
+			<Welcome name="Edite" />
+		</div>
+	)
+}
+
+const element = <Welcome name="Sara" />;
+const appElement = <App />;
+
+
+ReactDom.render(
+	appElement,
+	document.getElementById('root'),
+);
