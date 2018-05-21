f26a6481bbae7ef6332f5beebd84c767471b6dc2
ZiYuan <zhangziyuan@meicai.cn>
2018-05-21 16:31:03 +0800

    feat: 添加动态代码实验自制react

--- index.js
+++ index.js
@@ -68,8 +68,29 @@ const ReactDom = {
 	}
 }
 
-ReactDom.render(
-	<h2>Hello, world</h2>,
-	document.getElementById('root')
-)
+// ReactDom.render(
+// 	<h2>Hello, world</h2>,
+// 	document.getElementById('root')
+// )
+
+function tick() {
+	const element = (
+		<div>
+			<h1>Hello, world</h1>
+			<h2>It is {new Date().toLocaleTimeString()}.</h2>
+		</div>
+	);
+	ReactDom.render(
+		element,
+		document.getElementById('root'),
+	)
+}
+
+setInterval(tick, 1000);
+
+
+
+
+
+
 
